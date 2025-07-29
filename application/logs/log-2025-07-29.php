dd60b0e7d7d9c8c40db3913d21fa2b735aacc201
EmmanuelGaitann <118638965+EmmanuelGaitann@users.noreply.github.com>
2025-07-29 14:39:58 +0100

    fichier à faire

---
+++ application/logs/log-2025-07-29.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2025-07-29 13:35:40 --> Config Class Initialized
+INFO - 2025-07-29 13:35:40 --> Hooks Class Initialized
+DEBUG - 2025-07-29 13:35:40 --> UTF-8 Support Enabled
+INFO - 2025-07-29 13:35:40 --> Utf8 Class Initialized
+INFO - 2025-07-29 13:35:40 --> URI Class Initialized
+DEBUG - 2025-07-29 13:35:40 --> No URI present. Default controller set.
+INFO - 2025-07-29 13:35:40 --> Router Class Initialized
+INFO - 2025-07-29 13:35:40 --> Output Class Initialized
+INFO - 2025-07-29 13:35:40 --> Security Class Initialized
+DEBUG - 2025-07-29 13:35:40 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-07-29 13:35:40 --> Input Class Initialized
+INFO - 2025-07-29 13:35:40 --> Language Class Initialized
+INFO - 2025-07-29 13:35:41 --> Loader Class Initialized
+INFO - 2025-07-29 13:35:41 --> Helper loaded: url_helper
+INFO - 2025-07-29 13:35:41 --> Helper loaded: php8_helper
+INFO - 2025-07-29 13:35:41 --> Database Driver Class Initialized
+INFO - 2025-07-29 13:35:42 --> Parser Class Initialized
+INFO - 2025-07-29 13:35:42 --> Model "Impression_model" initialized
+INFO - 2025-07-29 13:35:42 --> Controller Class Initialized
+INFO - 2025-07-29 13:35:42 --> File loaded: C:\wamp64\www\candidature\application\views\template/header.php
+INFO - 2025-07-29 13:35:42 --> File loaded: C:\wamp64\www\candidature\application\views\start.php
+INFO - 2025-07-29 13:35:42 --> File loaded: C:\wamp64\www\candidature\application\views\template/footer.php
+INFO - 2025-07-29 13:35:42 --> Final output sent to browser
+DEBUG - 2025-07-29 13:35:42 --> Total execution time: 2.2514
